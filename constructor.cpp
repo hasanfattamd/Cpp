db1f48951c1f643cfd7220affa3aac8c2e0f4a92
hasanfattamd <hasanfattamd@gmail.com>
2024-10-18 21:02:40 +0530

    about cpp constructor

---
+++ constructor.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+
+using namespace std;
+
+class Complex
+{
+private:
+    int a, b;
+
+public:
+    Complex()
+    {
+    }
+    /*
+        Complex()
+        {
+            a = 0;
+            b = 0;
+        }
+        */
+    Complex(int x)
+    {
+        a = x;
+        b = 0;
+    }
+    Complex(int x, int y)
+    {
+        a = x;
+        b = y;
+    }
+
+    Complex(Complex &C)
+    {
+        a = C.a;
+        b = C.b;
+    }
+
+    void setData(int x, int y)
+    {
+        a = x;
+        b = y;
+    }
+    void showData()
+    {
+        cout << "a=" << a << " b=" << b << endl;
+    }
+
+    Complex sumGreater(Complex C)
+    {
+        if ((a + b) > (C.a + C.b))
+            return *this;
+        else
+            return C;
+    }
+    Complex add(Complex C)
+    {
+        Complex temp(8, 9);
+        temp.a = a + C.a;
+        temp.b = b + C.b;
+        return temp;
+    }
+};
+
+int main()
+{
+    Complex c1, c2(3, 5), c3(4);
+    Complex c4 = c2;
+    c1.showData();
+    c2.showData();
+    c3.showData();
+    c4.showData();
+    cout << endl;
+    return 0;
+}
